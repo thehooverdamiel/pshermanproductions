e5475850d742c9098e55b8adbd2f26ab1824fd63
Daniel Grimes <danielgrimes0@gmail.com>
2018-03-26 11:43:55 -0400

    add bootstrap

--- wp-content/themes/nikko-portfolio-child/functions.php
+++ wp-content/themes/nikko-portfolio-child/functions.php
@@ -3,6 +3,13 @@
 function enqueue_parent_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/build/theme-style.css' );
 }
+
+function enqueue_bootstrap_styles() {
+  wp_enqueue_style('bootstrap-style', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css', array());
+  wp_enqueue_script( 'bootstrap-script', get_stylesheet_directory_uri() . '/bootstrap/js/bootstrap.min.js', array('jquery'), '4.0.0', true );
+}
+
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_action( 'wp_enqueue_scripts', 'enqueue_bootstrap_styles');
 
 ?>
