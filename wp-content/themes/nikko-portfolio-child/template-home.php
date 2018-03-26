6b771f721d464f822e3b5fc65c9a3b37e6431b9e
Daniel Grimes <danielgrimes0@gmail.com>
2018-03-26 17:19:02 -0400

    home page basic finished

--- wp-content/themes/nikko-portfolio-child/template-home.php
+++ wp-content/themes/nikko-portfolio-child/template-home.php
@@ -20,9 +20,9 @@
     <!-- Start page main content -->
     
     <div class="container-fluid ">
-      <div class="row">
+      <div class="row bottom-buffer">
         <div class="col">
-          <p class="introduction">
+          <p>
             This area could be a fun introduction, like "he can do anything, he
             is the world's strongest man", etc. 
             Vivamus ornare mi diam, euismod aliquam urna imperdiet vitae. 
@@ -31,7 +31,7 @@
           </p>
         </div>
       </div>
-      <div class="row skills">
+      <div class="row skills bottom-buffer">
         <div class="col-xs-12 col-sm-6 col-lg-4">
           <!-- voice -->
           <div class="card">
@@ -84,14 +84,16 @@
           </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-xs-12 col-lg-4">
+      <div class="row about-mini">
+        <div class="col-xs-12 col-lg-4 text-center portrait">
+          <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/dad_and_charles.jpg'?>" alt="Picture of the world's strongest man">
         </div>
-        <div class="col-xs-12 col-lg-8">
+        <div class="col-xs-12 col-md-10 offset-md-1 offset-lg-0 col-lg-8">
           <p>
-            This area could have some kind of description of you and where you work
-            and live, like "the world's strongest man lives in Ypsilanti, MI, and has
-            a small and very cute dog." 
+            This area could have some kind of description of you and where you work/live, 
+            like "the world's strongest man lives in Ypsilanti, MI, and is 
+            capable of lifting two very big trains. 
+            He has an adorable dog named Charlie and a beautiful puff cat named Sam." 
             Vivamus ornare mi diam, euismod aliquam urna imperdiet vitae. 
             Suspendisse ullamcorper molestie feugiat. Integer sed consectetur leo.
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
